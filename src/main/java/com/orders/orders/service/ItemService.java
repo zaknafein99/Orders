92da26684a7aa503b01a69159f9e6663f89439e7
Ismael Marquez <ismael.marquez@globant.com>
2023-03-24 21:16:26 -0300

    Add delete item method

--- src/main/java/com/orders/orders/service/ItemService.java
+++ src/main/java/com/orders/orders/service/ItemService.java
@@ -2,12 +2,17 @@ package com.orders.orders.service;
 
 import com.orders.orders.domain.Item;
 import com.orders.orders.repository.ItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ItemService {
+
+    private final Logger logger = LoggerFactory.getLogger(ItemService.class);
 
     private final ItemRepository itemRepository;
 
@@ -21,5 +26,15 @@ public class ItemService {
 
     public List<Item> getAllItems(){
         return itemRepository.findAll();
+    }
+
+    public void deleteItem(Long itemId){
+        Optional<Item> item = itemRepository.findById(itemId);
+        if (item.isPresent()) {
+            itemRepository.delete(item.get());
+
+        } else {
+            logger.warn("Item with id {} not found", itemId);
+        }
     }
 }
