82cb4f8d074fa16cf125b8b23d14a9375a821f18
Ismael Marquez <670167+zaknafein99@users.noreply.github.com>
2023-04-05 19:03:27 -0300

    Order 5 (#9) (#10)
    * Add pageable to the getAllOrders and getAllCustomers results
    
    * Cleanup
    
    * Add docker files to start Postgres DB
    
    * Add delete item method
    
    * Add error handling in create order
    
    * Add delete OpenAPI Docs dependencies
    
    * Fix Open API version
    
    * Volume config change
    
    ---------
    
    Co-authored-by: Ismael Marquez <ismael.marquez@globant.com>

--- src/main/java/com/orders/orders/service/OrderService.java
+++ src/main/java/com/orders/orders/service/OrderService.java
@@ -1,41 +1,53 @@
 package com.orders.orders.service;
 
+import com.orders.orders.controller.CustomerController;
 import com.orders.orders.domain.Customer;
 import com.orders.orders.domain.Item;
 import com.orders.orders.domain.Order;
-import com.orders.orders.domain.Truck;
 import com.orders.orders.repository.ItemRepository;
 import com.orders.orders.repository.OrderRepository;
-import com.orders.orders.repository.TruckRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final TruckRepository truckRepository;
     private final CustomerService customerService;
     private final ItemRepository itemRepository;
 
-    public OrderService(OrderRepository orderRepository, TruckRepository truckRepository, CustomerService customerService, ItemRepository itemRepository) {
+    private static final Logger log = Logger.getLogger(CustomerController.class.getName());
+
+
+    public OrderService(OrderRepository orderRepository, CustomerService customerService, ItemRepository itemRepository) {
         this.orderRepository = orderRepository;
-        this.truckRepository = truckRepository;
         this.customerService = customerService;
         this.itemRepository = itemRepository;
     }
 
     public Order createOrder(Order order) {
-        Customer customer = customerService.getCustomerById(order.getCustomer().getId())
-                .orElseThrow(() -> new RuntimeException("Customer not found"));
-        order.setCustomer(customer);
+        try {
+            Customer customer = customerService.getCustomerById(order.getCustomer().getId())
+                    .orElseThrow(() -> new RuntimeException("Customer not found"));
+            order.setCustomer(customer);
+        } catch (RuntimeException e) {
+            // handle exception here
+            log.info("Customer with id " + order.getCustomer().getId() + " not found.");
+        }
 
         List<Item> items = new ArrayList<>();
-        for (Item item : order.getItems()) {
-            Item itemFromDb = itemRepository.findById(item.getId()).orElseThrow(() -> new RuntimeException("Item not found"));
-            items.add(itemFromDb);
+        try {
+            for (Item item : order.getItems()) {
+                Item itemFromDb = itemRepository.findById(item.getId()).orElseThrow(() -> new RuntimeException("Item not found"));
+                items.add(itemFromDb);
+            }
+        } catch (RuntimeException e) {
+            log.info("Item with id " + order.getCustomer().getId() + " not found.");
         }
         order.setItems(items);
         order.setTotalPrice(order.getTotalPrice());
@@ -44,16 +56,9 @@ public class OrderService {
         return orderRepository.save(order);
     }
 
-    public List<Order> getAllOrders(){
-        return orderRepository.findAll();
+    public Page<Order> getAllOrders(Pageable pageable){
+        return orderRepository.findAll(pageable);
     }
-
-    private Truck findAvailableTruck(Double price) {
-        // logic for finding available truck
-        return null;
-    }
-
-
 
     // other methods for handling orders
 }
